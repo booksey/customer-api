23566b71e4247dbc72ed13bf27631091ebaa9a19
booksey <booksey87@gmail.com>
2023-08-18 00:09:22 +0200

    routes fixed

--- config/route.php
+++ config/route.php
@@ -8,10 +8,10 @@ use Slim\Interfaces\RouteCollectorProxyInterface;
 return function (SlimApp $app): void {
     /** Index group */
     $app->get('[/]', \App\Action\IndexAction::class)->setName('index');
-    $app->group('/api/Customer', function (RouteCollectorProxyInterface $app) {
-        $app->get('/get', \App\Action\Api\Customer\GetAction::class)->setName('Customer:get');
-        $app->get('/create', \App\Action\Api\Customer\CreateAction::class)->setName('Customer:create');
-        $app->get('/update', \App\Action\Api\Customer\UpdateAction::class)->setName('Customer:update');
-        $app->get('/delete', \App\Action\Api\Customer\DeleteAction::class)->setName('Customer:delete');
+    $app->group('/api/customer', function (RouteCollectorProxyInterface $app) {
+        $app->post('/get', \App\Action\Api\Customer\GetAction::class)->setName('customer:get');
+        $app->post('/create', \App\Action\Api\Customer\CreateAction::class)->setName('customer:create');
+        $app->post('/update', \App\Action\Api\Customer\UpdateAction::class)->setName('customer:update');
+        $app->post('/delete', \App\Action\Api\Customer\DeleteAction::class)->setName('customer:delete');
     });
 };
